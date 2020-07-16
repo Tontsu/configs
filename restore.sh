d20de0a7d5d920002135c1e397ea7ff9f242c39c
Tontsu <tontsu@pop-os.localdomain>
2020-07-16 14:51:29 +0300

    initial

---
+++ restore.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp MangoHud.conf /home/$USER/.config/MangoHud/
+cp .bashrc /home/$USER/
+cp autoexec.cfg /home/$USER/.steam/debian-installation/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/cfg
